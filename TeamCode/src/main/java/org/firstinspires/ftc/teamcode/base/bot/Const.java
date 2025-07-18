b822cc39e29f26d37fb134eb38c3ea7a7fbfa600
NazLovesLifting <nazimayub786@gmail.com>
2025-07-17 20:13:03 -0500

    cd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/bot/Const.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/base/bot/Const.java
@@ -22,7 +22,7 @@ public class Const {
 
     //Outtake
     public static double outtakeClawDistTempTransfer = -1, outtakeClawDistRightInitTransfer = 1, outtakeClawDistLeftInitTransfer = 0, outtakeClawDistNew = 0.7, outtakeClawDistRightFinalTransfer = -1, outtakeClawDistLeftFinalTransfer = -1, outtakeClawRotInitTransfer = 0.347, outtakeClawRotTransfer = .505,
-            release = 0.5, grab = .32,
+            release = 0.5, grab = .13,
             distBasketPos = .4, rotBasketPos = 0,
             distSpecimenGrab = 0.01, rotSpecimenGrab = .916, distSpecimenGrabFinal = .785, rotSpecimenScore = 0.6, distSpecimenScore = .786;
 
